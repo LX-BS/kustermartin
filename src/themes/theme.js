6b047543bbec1f32dd8c98be8da9900ed9bf8044
LX-BS <a@b-netz.ch>
2021-08-05 17:15:19 +0200

    Add styled components

---
+++ src/themes/theme.js
@@ -0,0 +1,29 @@
+export default {
+    fonts: {
+        main: "Inter, sans-serif",
+        code: "Roboto Rono, monospace",
+    },
+    colors: {
+        main1: "hsl(0,50%,95%)",
+        main2: "hsl(315,80%,55%)",
+        dark: "hsl(0,0%,20%)",
+        light: "hsl(0,0%,95%)",
+    },
+    breakpoints: {
+        mobile: "only screen and (max-width: 50rem)",
+        tablet: "only screen and (max-width: 65rem)",
+    },
+    spacings: {
+        small: ".5rem",
+        medium: "1.5rem",
+        large: "3rem",
+    },
+    animations: {
+        button: "box-shadow 0.3s ease",
+        link: "color 0.2s ease",
+    },
+    shadows: {
+        shadow1: "0px 5px 20px rgba(30, 30, 31, 0.05)",
+    },
+
+}
