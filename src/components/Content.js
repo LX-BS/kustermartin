1fc75c07c59330fec631d78efe3a15aab4e04b0c
LX-BS <a@b-netz.ch>
2021-08-16 15:33:29 +0200

    Add Content

---
+++ src/components/Content.js
@@ -0,0 +1,6 @@
+import React from "react"
+import { ContainerWrapper, ContentWrapper } from "../elements"
+
+export const Content = ({ children }) => {
+  return <ContainerWrapper>{children}</ContainerWrapper>
+}
